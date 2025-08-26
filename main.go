264953ebffc86a4413848e3153a80c3b8ebfee43
omar0ali <bajunaidomar@gmail.com>
2025-08-26 12:45:26 -0400

    Implementation of screen, gamecontext and a spaceship entity on screen. Ensure game updates when moving the spaceship works.

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 
 import (
 	"github.com/gdamore/tcell/v2"
+	"github.com/omar0ali/spaceinvader-game-cli/core"
+	"github.com/omar0ali/spaceinvader-game-cli/entities"
 	"github.com/omar0ali/spaceinvader-game-cli/window"
 )
 
@@ -11,9 +13,20 @@ func main() {
 
 	// window by default is set to 30 FPS
 	// window.InitScreen(window.ChangeTickerDuration(16)) // this can update the framerate to 60
-	window.InitScreen()
-
+	screen := window.InitScreen()
+	w, h := window.GetSize()
 	window.SetTitle("Space Invader Game")
+	// ------------------------------------- Objects ----------------------------------
+	gameContext := core.GameContext{
+		Screen: screen,
+	}
+	// ---------------------------------- entities --------------------------------------
+	spaceship := entities.InitSpaceShip(core.Point{
+		X: w / 2,
+		Y: h - 4,
+	})
+
+	gameContext.AddEntity(spaceship)
 
 	window.InputEvent(exit,
 		func(event tcell.Event) {
@@ -23,6 +36,9 @@ func main() {
 					// testing exit with letter r
 					exit <- 0
 				}
+				for _, entity := range gameContext.GetEntities() {
+					entity.InputEvents(event, &gameContext)
+				}
 			}
 		},
 	)
@@ -30,6 +46,10 @@ func main() {
 	window.Update(exit,
 		func(delta float64) {
 			// update game
+			for _, entity := range gameContext.GetEntities() {
+				entity.Draw(&gameContext)
+				entity.Update(&gameContext)
+			}
 		},
 	)
 
