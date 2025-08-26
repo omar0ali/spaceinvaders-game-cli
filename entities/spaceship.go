e06c839065b3a021cc74ac8c7767df7f9755c48a
omar0ali <bajunaidomar@gmail.com>
2025-08-26 14:46:35 -0400

    Redesign the spaceship, replaced spaceship movements using the mouse motions

--- entities/spaceship.go
+++ entities/spaceship.go
@@ -8,72 +8,56 @@ import (
 )
 
 type SpaceShip struct {
-	points struct {
-		A, B, C core.Point
-	}
-	origin core.Point
+	triangle core.Triangle
+	origin   core.Point
 }
 
-func InitSpaceShip(origin core.Point) *SpaceShip {
-	return &SpaceShip{
+// init in the bottom center of the secreen by default
+
+func InitSpaceShip() SpaceShip {
+	w, h := window.GetSize()
+	origin := core.Point{
+		X: w / 2,
+		Y: h - 2,
+	}
+	return SpaceShip{
 		origin: origin,
-		points: struct {
-			A, B, C core.Point
-		}{
+		triangle: core.Triangle{
 			A: core.Point{X: origin.X, Y: origin.Y - 1},
-			B: core.Point{X: origin.X - 1, Y: origin.Y + 1}, // left
-			C: core.Point{X: origin.X + 1, Y: origin.Y + 1}, // right
+			B: core.Point{X: origin.X - 2, Y: origin.Y + 1}, // left
+			C: core.Point{X: origin.X + 2, Y: origin.Y + 1}, // right
 		},
 	}
 }
 
 // TODO: will need to use delta to ensure smooth movements
+// delta can be used with window.GetDelta()
 
 func (s *SpaceShip) Update(gs *core.GameContext) {}
 
 func (s *SpaceShip) Draw(gs *core.GameContext) {
-	gs.Screen.SetContent(s.points.A.X, s.points.A.Y, '*', nil, window.GetStyle())
-	gs.Screen.SetContent(s.points.B.X, s.points.B.Y, '*', nil, window.GetStyle()) // left
-	gs.Screen.SetContent(s.points.C.X, s.points.C.Y, '*', nil, window.GetStyle()) // right
+	gs.Screen.SetContent(s.triangle.A.X, s.triangle.A.Y, '^', nil, window.GetStyle())
+	gs.Screen.SetContent(s.triangle.B.X, s.triangle.B.Y, '*', nil, window.GetStyle()) // left
+	gs.Screen.SetContent(s.triangle.C.X, s.triangle.C.Y, '*', nil, window.GetStyle()) // right
+	// left line
+	gs.Screen.SetContent(s.triangle.A.X-1, s.triangle.A.Y+1, '/', nil, window.GetStyle())
+	// right line
+	gs.Screen.SetContent(s.triangle.A.X+1, s.triangle.A.Y+1, '\\', nil, window.GetStyle())
+	// bottom line
+	gs.Screen.SetContent(s.triangle.A.X, s.triangle.A.Y+2, tcell.RuneS7, nil, window.GetStyle())
 }
 
 func (s *SpaceShip) InputEvents(event tcell.Event, gc *core.GameContext) {
+	moveMouse := func(x int) {
+		s.origin.X = x
+		s.triangle.A.X = x
+		s.triangle.B.X = x + 2
+		s.triangle.C.X = x - 2
+	}
 	switch ev := event.(type) {
-	case *tcell.EventKey:
-		screen := gc.Screen
-		w, _ := screen.Size()
-
-		// TODO: Refactor
-
-		// restriction
-		if s.points.B.X < 1 {
-			s.origin.X += 2
-			s.points.A.X += 2
-			s.points.B.X += 2
-			s.points.C.X += 2
-			return
-		}
-		if s.points.C.X > w-1 {
-			s.origin.X -= 2
-			s.points.A.X -= 2
-			s.points.B.X -= 2
-			s.points.C.X -= 2
-			return
-		}
-
-		// controls
-		if ev.Key() == tcell.KeyLeft {
-			s.origin.X -= 2
-			s.points.A.X -= 2
-			s.points.B.X -= 2
-			s.points.C.X -= 2
-		}
-		if ev.Key() == tcell.KeyRight {
-			s.origin.X += 2
-			s.points.A.X += 2
-			s.points.B.X += 2
-			s.points.C.X += 2
-		}
+	case *tcell.EventMouse:
+		x, _ := ev.Position()
+		moveMouse(x)
 	}
 }
 
