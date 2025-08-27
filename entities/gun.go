495ec1cb061d17cb5955edf3a14854fa0af1dce3
omar0ali <bajunaidomar@gmail.com>
2025-08-27 19:56:15 -0400

    Finished implemented a gun, it shoots by left mouse click.

--- entities/gun.go
+++ entities/gun.go
@@ -3,6 +3,7 @@ package entities
 import (
 	"github.com/gdamore/tcell/v2"
 	"github.com/omar0ali/spaceinvader-game-cli/core"
+	"github.com/omar0ali/spaceinvader-game-cli/window"
 )
 
 type Beam struct {
@@ -17,29 +18,59 @@ type Gun struct {
 
 func (g *Gun) initBeam(speed int, pos core.Point) {
 	beam := Beam{
-		speed, pos, 'X',
+		speed,
+		core.Point{
+			X: pos.X,
+			Y: pos.Y - 2,
+		},
+		tcell.RuneDiamond,
 	}
 	g.Beams = append(g.Beams, &beam)
 }
 
 func (g *Gun) Update(gc *core.GameContext, delta float64) {
 	// update the coordinates of the beam
+	if len(g.Beams) < 1 {
+		return
+	}
+
+	// Temporary slice for alive beams
+	var activeBeams []*Beam
+
+	for _, beam := range g.Beams {
+		distance := int(float64(beam.speed) * delta)
+		beam.position.Y -= distance
+
+		// Only keep beam if it's still on screen
+		if beam.position.Y >= 0 {
+			activeBeams = append(activeBeams, beam)
+		}
+	}
+
+	// Replace old list with filtered one
+	g.Beams = activeBeams
 }
 
 func (g *Gun) Draw(gc *core.GameContext) {
 	// draw the beam new position
+	if len(g.Beams) < 1 {
+		return
+	}
+	for _, beam := range g.Beams {
+		window.SetContent(beam.position.X, beam.position.Y, beam.symbol)
+	}
 }
 
 func (g *Gun) InputEvents(event tcell.Event, gc *core.GameContext) {
 	// on click, will create a new beam
 	switch ev := event.(type) {
-	case *tcell.EventKey:
-		if ev.Key() == ' ' {
+	case *tcell.EventMouse:
+		if ev.Buttons() == tcell.Button1 {
 			for _, entity := range gc.GetEntities() {
 				if entity.GetType() == "spaceship" {
 					spaceShip, ok := entity.(*SpaceShip)
 					if ok {
-						g.initBeam(10, spaceShip.origin)
+						g.initBeam(50, spaceShip.origin)
 						break
 					}
 				}
