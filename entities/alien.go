11156aa627a5d2fa00763d425844f3e518336848
omar0ali <bajunaidomar@gmail.com>
2025-08-28 15:35:12 -0400

    able to create an aline and display it in the middle of the screen...
    Alien spaceship looks alright for now. Player spacehship beams able to
    hit and distroy. aline ship has health, needs to delpet until the ship
    is destryed. Beam has power which is used to decrease the health of the
    alien's health

--- entities/alien.go
+++ entities/alien.go
@@ -36,6 +36,24 @@ func (a *AlienProducer) AddAlien(health, speed int, origin core.Point) {
 	a.aliens = append(a.aliens, &alien)
 }
 
+func (a *alien) IsHit(point core.Point) bool {
+	if point == a.triangle.A ||
+		point == a.triangle.B ||
+		point == a.triangle.C ||
+		point.X == a.origin.X+1 && point.Y == a.origin.Y ||
+		point.X == a.origin.X-1 && point.Y == a.origin.Y {
+		window.SetContent(point.X-1, point.Y+1, 'X')
+		window.SetContent(point.X-1, point.Y, 'X')
+		window.SetContent(point.X+1, point.Y, 'X')
+		window.SetContent(point.X, point.Y+1, 'X')
+		window.SetContent(point.X, point.Y-1, 'X')
+		window.SetContent(point.X+1, point.Y+1, 'X')
+		return true
+	}
+
+	return false
+}
+
 type AlienProducer struct {
 	aliens []*alien
 }
@@ -46,28 +64,58 @@ func InitAlienProducer() AlienProducer {
 	}
 }
 
-func (a *AlienProducer) CheckAliensHealth() {
+func (a *AlienProducer) CheckAliensHealth(gc *core.GameContext) {
 	var activeAliens []*alien
-	for _, a := range a.aliens {
-		if a.health > 0 { // only add aliens with high health
-			// this should ensure dead aliens are untracked can be gone with GC
-			activeAliens = append(activeAliens, a)
+	var beams []*Beam
+	// look for the spaceship since it has the gun and the number of beams
+	for _, entity := range gc.GetEntities() {
+		if entity.GetType() == "spaceship" {
+			if spaceship, ok := entity.(*SpaceShip); ok {
+				beams = spaceship.Gun.Beams
+			}
+			break
 		}
 	}
+	// on each alien avaiable check its position and check if the beam is at the same position
+	for _, alien := range a.aliens {
+		for _, beam := range beams {
+			if alien.IsHit(beam.position) {
+				alien.health -= beam.power
+			}
+		}
+
+		// check the health of each alien
+		if alien.health > 0 {
+			activeAliens = append(activeAliens, alien)
+		}
+	}
+
 	a.aliens = activeAliens
 }
 
 func (a *AlienProducer) Update(gc *core.GameContext, delta float64) {
 	// Update the coordinates of the aliens.
 	// should create alines and place them in random positions on screen.
-	a.CheckAliensHealth() // this will ensure to clean up dead aliens
+	a.CheckAliensHealth(gc) // this will ensure to clean up dead aliens
 }
 
 func (a *AlienProducer) Draw(gc *core.GameContext) {
 	for _, alien := range a.aliens {
+		// drawing the points
 		window.SetContent(alien.triangle.A.X, alien.triangle.A.Y, '*')
 		window.SetContent(alien.triangle.B.X, alien.triangle.B.Y, '*')
 		window.SetContent(alien.triangle.C.X, alien.triangle.C.Y, '*')
+		// lines bellow
+		window.SetContent(alien.triangle.C.X+1, alien.triangle.C.Y, '-')
+		window.SetContent(alien.triangle.B.X-1, alien.triangle.C.Y, '-')
+		window.SetContent(alien.triangle.C.X+2, alien.triangle.C.Y, '-')
+		window.SetContent(alien.triangle.B.X-2, alien.triangle.C.Y, '-')
+		// lines left
+		window.SetContent(alien.triangle.B.X-1, alien.triangle.B.Y+1, '/')
+		window.SetContent(alien.triangle.B.X-2, alien.triangle.B.Y+2, '/')
+		// lines right
+		window.SetContent(alien.triangle.C.X+1, alien.triangle.B.Y+1, '\\')
+		window.SetContent(alien.triangle.C.X+2, alien.triangle.B.Y+2, '\\')
 	}
 }
 
@@ -77,7 +125,7 @@ func (a *AlienProducer) InputEvents(event tcell.Event, gc *core.GameContext) {
 		if ev.Rune() == ' ' {
 			// testing spawn an alinen
 			w, h := gc.Screen.Size()
-			a.AddAlien(10, 10, core.Point{
+			a.AddAlien(300, 10, core.Point{
 				X: w / 2,
 				Y: (h / 2) - 5,
 			})
