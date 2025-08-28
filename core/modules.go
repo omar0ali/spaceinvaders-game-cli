775c21c7e04af7d217f6bad7db638fc9d81e3d25
omar0ali <bajunaidomar@gmail.com>
2025-08-28 18:21:32 -0400

    Using PointFloat and Point and making an interface so I can switch and choose between these too
    Needed to use floatPoint for alien movement so its smoother and use the
    correct speed and delta

--- core/modules.go
+++ core/modules.go
@@ -4,6 +4,67 @@ type Point struct {
 	X, Y int
 }
 
+type PointFloat struct {
+	X, Y float64
+}
+
+type PointInterface interface {
+	GetX() float64
+	GetY() float64
+	SetX(float64)
+	SetY(float64)
+	AppendX(float64)
+	AppendY(float64)
+}
+
+func (p Point) GetX() float64 {
+	return float64(p.X)
+}
+
+func (p Point) GetY() float64 {
+	return float64(p.Y)
+}
+
+func (p *Point) SetX(x float64) {
+	p.X = int(x)
+}
+
+func (p *Point) SetY(y float64) {
+	p.Y = int(y)
+}
+
+func (p *Point) AppendX(x float64) {
+	p.X += int(x)
+}
+
+func (p *Point) AppendY(y float64) {
+	p.Y += int(y)
+}
+
+func (p PointFloat) GetX() float64 {
+	return p.X
+}
+
+func (p PointFloat) GetY() float64 {
+	return p.Y
+}
+
+func (p *PointFloat) SetX(x float64) {
+	p.X = x
+}
+
+func (p *PointFloat) SetY(y float64) {
+	p.Y = y
+}
+
+func (p *PointFloat) AppendX(x float64) {
+	p.X += x
+}
+
+func (p *PointFloat) AppendY(y float64) {
+	p.Y += y
+}
+
 type Triangle struct {
-	A, B, C Point
+	A, B, C PointInterface
 }
