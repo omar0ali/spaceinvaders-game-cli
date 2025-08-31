0a50ac91a06bc4a367f2e1aae071f1d69cf514ae
omar0ali <bajunaidomar@gmail.com>
2025-08-31 19:35:01 -0400

    Implementing RemoveAllEntities func used when restarting the game, should clear everything and start over

--- core/game.go
+++ core/game.go
@@ -1,7 +1,9 @@
 // Package core
 package core
 
-import "github.com/gdamore/tcell/v2"
+import (
+	"github.com/gdamore/tcell/v2"
+)
 
 type (
 	Entity interface {
@@ -17,25 +19,29 @@ type (
 	}
 )
 
-func (gs *GameContext) AddEntity(entity ...Entity) {
-	gs.entities = append(gs.entities, entity...)
+func (gc *GameContext) AddEntity(entity ...Entity) {
+	gc.entities = append(gc.entities, entity...)
 }
 
-func (gs *GameContext) RemoveEntity(entity Entity) {
-	for i, v := range gs.entities {
+func (gc *GameContext) RemoveEntity(entity Entity) {
+	for i, v := range gc.entities {
 		if v == entity {
-			gs.entities = append(gs.entities[:i], gs.entities[i+1:]...)
+			gc.entities = append(gc.entities[:i], gc.entities[i+1:]...)
 			return
 		}
 	}
 }
 
-func (gs *GameContext) GetEntities() []Entity {
-	return gs.entities
+func (gc *GameContext) RemoveAllEntities() {
+	gc.entities = []Entity{}
 }
 
-func (gs *GameContext) FindEntity(typeName string) Entity {
-	for _, entity := range gs.entities {
+func (gc *GameContext) GetEntities() []Entity {
+	return gc.entities
+}
+
+func (gc *GameContext) FindEntity(typeName string) Entity {
+	for _, entity := range gc.entities {
 		if entity.GetType() == typeName {
 			return entity
 		}
