7cb36091de4f2a2d469f6c8560cb320bea05e430
omar0ali <bajunaidomar@gmail.com>
2025-09-07 13:55:35 -0400

    Removing Triangle Point. Update IsHit() to have a nice effect when beam hit objects. Adding Options when creating a new Object

--- entities/objects.go
+++ entities/objects.go
@@ -10,7 +10,7 @@ type FallingObjectBase struct {
 	Health        int
 	Speed         int
 	OriginPoint   core.PointFloat
-	TrianglePoint core.Triangle
+	Width, Height int
 }
 
 func (f *FallingObjectBase) isOffScreen(h int) bool {
@@ -21,29 +21,42 @@ func (f *FallingObjectBase) isDead() bool {
 	return f.Health <= 0
 }
 
-func (f *FallingObjectBase) isHit(point core.PointInterface, power int) bool {
+func (f *FallingObjectBase) isHit(pointBeam core.PointInterface, power int) bool {
 	grayColor := window.StyleIt(tcell.ColorReset, tcell.ColorDarkGray)
 	redColor := window.StyleIt(tcell.ColorReset, tcell.ColorRed)
 	yellowColor := window.StyleIt(tcell.ColorReset, tcell.ColorYellow)
 
-	if f.TrianglePoint.A.GetY() > point.GetY() &&
-		f.TrianglePoint.C.GetY()-2 < point.GetY() &&
-		(f.TrianglePoint.C.GetX()-1 < point.GetX() && f.TrianglePoint.B.GetX()+1 > point.GetX()) {
+	// draw flash when hitting
+	pattern := []struct {
+		dx, dy int
+		r      rune
+		style  tcell.Style
+	}{
+		{-1, 0, tcell.RuneBoard, yellowColor},
+		{1, 0, tcell.RuneBoard, yellowColor},
+		{0, -1, tcell.RuneBoard, grayColor},
+		{0, 1, tcell.RuneBoard, grayColor},
+		{-1, -1, tcell.RuneCkBoard, grayColor},
+		{1, -1, tcell.RuneCkBoard, grayColor},
+		{-1, 1, tcell.RuneCkBoard, redColor},
+		{1, 1, tcell.RuneBoard, grayColor},
+	}
+
+	if pointBeam.GetX() >= f.OriginPoint.X-float64(f.Width) &&
+		pointBeam.GetX() <= f.OriginPoint.X+float64(f.Width) &&
+		pointBeam.GetY() >= f.OriginPoint.Y-float64(f.Height) &&
+		pointBeam.GetY() <= f.OriginPoint.Y+float64(f.Height) {
 
 		f.Health -= power // update health of the falling object
 
-		window.SetContentWithStyle(
-			int(point.GetX()-1), int(point.GetY()+1), tcell.RuneBoard, grayColor)
-		window.SetContentWithStyle(
-			int(point.GetX()-1), int(point.GetY()), tcell.RuneCkBoard, yellowColor)
-		window.SetContentWithStyle(
-			int(point.GetX()+1), int(point.GetY()), tcell.RuneBoard, grayColor)
-		window.SetContentWithStyle(
-			int(point.GetX()), int(point.GetY()+1), tcell.RuneCkBoard, redColor)
-		window.SetContentWithStyle(
-			int(point.GetX()), int(point.GetY()-1), tcell.RuneBoard, yellowColor)
-		window.SetContentWithStyle(
-			int(point.GetX()+1), int(point.GetY()+1), tcell.RuneCkBoard, grayColor)
+		for _, p := range pattern {
+			window.SetContentWithStyle(
+				int(pointBeam.GetX())+p.dx,
+				int(pointBeam.GetY())+p.dy,
+				p.r, p.style,
+			)
+		}
+
 		return true
 	}
 	return false
@@ -52,30 +65,23 @@ func (f *FallingObjectBase) isHit(point core.PointInterface, power int) bool {
 func (f *FallingObjectBase) move(delta float64) {
 	distance := float64(f.Speed) * delta
 	f.OriginPoint.Y += distance
-	f.TrianglePoint.A.AppendY(distance)
-	f.TrianglePoint.B.AppendY(distance)
-	f.TrianglePoint.C.AppendY(distance)
+	f.OriginPoint.AppendY(distance)
 }
 
-func NewObject(health, speed int, origin core.PointFloat) *FallingObjectBase {
+type ObjectOpts struct {
+	Health        int
+	Speed         int
+	OriginPoint   core.PointFloat
+	Width, Height int
+}
+
+func NewObject(opts ObjectOpts) *FallingObjectBase {
 	return &FallingObjectBase{
-		Health:      health,
-		Speed:       speed,
-		OriginPoint: origin,
-		TrianglePoint: core.Triangle{
-			A: &core.PointFloat{
-				X: origin.X,
-				Y: origin.Y + 2,
-			},
-			B: &core.PointFloat{
-				X: origin.X + 3,
-				Y: origin.Y,
-			},
-			C: &core.PointFloat{
-				X: origin.X - 3,
-				Y: origin.Y,
-			},
-		},
+		Health:      opts.Health,
+		Speed:       opts.Speed,
+		OriginPoint: opts.OriginPoint,
+		Height:      opts.Height,
+		Width:       opts.Width,
 	}
 }
 
