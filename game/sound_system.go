f74980f8cd677f545f87283e3d20f26bb839af4d
Omar BaGunaid <bajunaidomar@gmail.com>
2025-11-07 19:12:51 -0500

    Fix and Improvments: Ensure sounds play with no lag, and limit loaded sounds to 10 at a time.
    This should ensure that sounds are played without hiccups and lags

--- game/sound_system.go
+++ game/sound_system.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"io"
 	"math/rand"
+	"sync/atomic"
 	"time"
 
 	"github.com/gopxl/beep/v2"
@@ -11,6 +12,12 @@ import (
 	"github.com/gopxl/beep/v2/mp3"
 	"github.com/gopxl/beep/v2/speaker"
 	"github.com/omar0ali/spaceinvaders-game-cli/game/assets"
+)
+
+// to limit how many sounds playing at the same time.
+var (
+	soundsPlaying int32
+	maxSounds     int32 = 10
 )
 
 type nopCloser struct {
@@ -46,6 +53,10 @@ func InitSoundSystem(cfg GameConfig) *SoundSystem {
 		sounds[name] = Sound{Data: data, Format: format}
 	}
 
+	// prepare speaker only once.
+	sampleRate := beep.SampleRate(44100) // usign mp3
+	speaker.Init(sampleRate, sampleRate.N(time.Second/10))
+
 	return &SoundSystem{
 		Sounds: sounds,
 		cfg:    cfg,
@@ -69,11 +80,17 @@ func (s *SoundSystem) PlaySound(name string, vol float64) {
 		Log(Error, "Failed to locate the file. %s ", name)
 	}
 
+	if atomic.LoadInt32(&soundsPlaying) >= maxSounds {
+		Log(Debug, "Skipping sound: too many playing")
+		return
+	}
+
 	Log(Debug, "Sounds Playing: %s", name)
 
 	go func() {
+		atomic.AddInt32(&soundsPlaying, 1)
 		r := nopCloser{bytes.NewReader(sound.Data)}
-		streamer, format, err := mp3.Decode(r)
+		streamer, _, err := mp3.Decode(r)
 		if err != nil {
 			Log(Error, "Failed to decode %s", err)
 			return
@@ -85,9 +102,9 @@ func (s *SoundSystem) PlaySound(name string, vol float64) {
 			Volume:   vol,
 			Silent:   false,
 		}
-		speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
 		speaker.Play(beep.Seq(v, beep.Callback(func() {
 			streamer.Close()
+			atomic.AddInt32(&soundsPlaying, -1)
 		})))
 	}()
 }
